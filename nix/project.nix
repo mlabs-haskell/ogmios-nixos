359b1eeb2d8bbc23119ce42c69b1fe5bc588eb29
klarkc <walkerleite490@gmail.com>
2024-02-19 16:44:32 -0300

    Add ogmios flags

--- nix/project.nix
+++ nix/project.nix
@@ -14,11 +14,15 @@
       inputMap = {
         "https://input-output-hk.github.io/cardano-haskell-packages" = inputs.iogx.inputs.CHaP;
       };
-      modules = [
-        {
-          # FIXME ogmios unit tests are not passing
-          packages.ogmios.components.tests.unit.doCheck = false;
-        }
-      ];
+      #sha256map = {};
+      modules = [{
+        packages.cryptonite.flags.support_rdrand = false;
+        packages.bitvec.flags.simd = false;
+        packages.quickcheck-state-machine.flags.no-vendored-treediff = true;
+        packages.text.flags.simdutf = false;
+        packages.formatting.flags.no-double-conversion = true;
+        # FIXME ogmios unit tests are not passing
+        packages.ogmios.components.tests.unit.doCheck = false;
+      }];
     };
 }
